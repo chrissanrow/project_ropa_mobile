d2343c1a90b57c04c355f7cc9e81fdc75201aad1
chrissanrow <chris.sanrow@gmail.com>
2025-05-17 12:22:22 -0700

    Basic nav completed for user view

---
+++ app/donations/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const DonationDetails = () => {
+  return (
+    <View>
+      <Text>DonationDetails</Text>
+    </View>
+  )
+}
+
+export default DonationDetails
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        textprimary: "#030014",
+        textsecondary: "#151312",
+        primary: "#224869",
+        secondary: "#FD7646",
+        lightprimary: {
+          100: "#bcc8d2",
+          200: "#93a4b4",
+          300: "#6b8196"
+        },
+        lightsecondary: {
+          100: "#fecebd",
+          200: "#feae93",
+          300: "#fd8f68"
+        },
+        darkprimary: {
+          100: "#1a3e5f",
+          200: "#163552",
+          300: "#132539"
+        },
+        background: "#e5e9ec"
+      }
+    },
+  },
+  plugins: [],
+}
