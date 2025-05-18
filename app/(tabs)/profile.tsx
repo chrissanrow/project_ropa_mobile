e5cc5a12503200da44f6a250f3eaf457543a84f1
youdonthavetobeprettyanymore <youdonthavetobeprettyanymore@gmail.com>
2025-05-17 18:22:08 -0700

    fix sessions.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,26 +1,27 @@
-import { supabase } from '@/lib/supabase';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { supabase } from "@/lib/supabase";
+import React from "react";
+import { Button, Text, View } from "react-native";
 
 const profile = () => {
   async function handleLogout() {
     try {
+      await supabase.auth.refreshSession();
       const { error } = await supabase.auth.signOut();
+
+      await supabase.auth.refreshSession();
       if (error) throw error;
       // message = 'Logged out successfully.'; // Store will update UI
     } catch (error) {
-      console.error('Error logging out:', error);
+      console.error("Error logging out:", error);
     }
   }
 
   return (
     <View className="flex-1 justify-center items-center">
       <Text>profile</Text>
-      <Button
-        title="Logout"
-        onPress={handleLogout}></Button>
+      <Button title="Logout" onPress={handleLogout}></Button>
     </View>
-  )
-}
+  );
+};
 
-export default profile
+export default profile;
